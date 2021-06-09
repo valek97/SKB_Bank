d31e5b5a1d8a8854ae170dcfc82e60dd00248e76
valek97 <kritenko97@gmail.com>
2021-06-09 15:54:50 +0300

    Компановка js файлов, поддержка: контроллеров и представлений, статических файлов, ендпоинтов

--- OrderList/Startup.cs
+++ OrderList/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,15 +13,18 @@ namespace OrderList
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Поддержка контролллера MVC
+            services.AddControllersWithViews()
+                //Совместимость с кор 3.0
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Порядок регистрации данных в midleware
+
+            //Подробности об ошибках
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -28,12 +32,13 @@ namespace OrderList
 
             app.UseRouting();
 
+            //Подключение статичных файлов в приложении
+            app.UseStaticFiles();
+
+            //Регистрация нужного маршрута(ендпоинт)
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
